9d07e44160f8dc4516b367030c62dedc2c7b2a9d
Chaciga Lulu <echaciga@gmail.com>
2020-04-12 08:40:01 +0300

    start building estimator

--- src/estimator.php
+++ src/estimator.php
@@ -1,6 +1,26 @@
 <?php
+  
+  $region = array("name" => "Africa", "avgAge" => 19.7, "avgDailyIncomeInUSD" => 5, "avgDailyIncomePopulation" => 0.71);
+  $periodType = "days";
+  $timeToElapse = 58;
+  $reportedCases = 674;
+  $population = 66622705;
+  $totalHospitalBeds = 1380614;
 
-function covid19ImpactEstimator($data)
-{
-  return $data;
-}
+  function covid19ImpactEstimator($reportedCases, $timeToElapse, $population, $totalHospitalBeds){
+
+    function impact($reportedCases, $timeToElapse){
+    $currentlyInfected = $reportedCases * 10;
+      $infectionsByRequestedTime = $currentlyInfected * pow(2, ($timeToElapse / 3));
+  //    $severeCasesByRequestedTime = $infectionsByRequestedTime * (15/100);
+    } function impact($reportedCases, $timeToElapse);
+
+    function severeImpact($reportedCases, $timeToElapse){
+      $currentlyInfected = $reportedCases * 50;
+         $infectionsByRequestedTime = $currentlyInfected * pow(2, ($timeToElapse / 3));
+  //       $severeCasesByRequestedTime = $infectionsByRequestedTime * (15/100);
+
+    }severeImpact($reportedCases, $timeToElapse);
+  }covid19ImpactEstimator($reportedCases, $timeToElapse, $population, $totalHospitalBeds);
+  
+  ?>
